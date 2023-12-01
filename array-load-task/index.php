409fff963543a043a8f250d94f105e50c7255840
nicoseper89 <nicosepertino@hotmail.com>
2023-11-30 21:24:51 -0300

    refactor

--- array-load-task/index.php
+++ array-load-task/index.php
@@ -18,23 +18,25 @@
 
         foreach ($games as $i => $game) {
 
-            echo '
-                <div class="col-sm-3 m-0 p-0">
+            ?> 
+            
+            <div class="col-sm-3 m-0 p-0">
                     <div class="card h-100">
                         <div class="relative overflow-x-hidden h-75">
-                            <img class="card-img-top h-100" src="' . $game['image'] . '" alt="Card image ' . $i . '">
+                            <img class="card-img-top h-100" src="<?php echo $game['image'] ?>" alt="Card image ' . $i . '">
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">' . $game['name'] . '</h5>
-                            <p class="card-text">Consola: ' . $game['console'] . '</p>
-                            <p class="card-text">Categoría: ' . $game['category'] . '</p>
-                            <p class="card-text"><small class="text-muted">Lanzamiento: ' . $game['release'] . '</small></p>
+                            <h5 class="card-title"><?php echo $game['name'] ?></h5>
+                            <p class="card-text">Consola: <?php echo $game['console'] ?></p>
+                            <p class="card-text">Categoría: <?php echo $game['category'] ?></p>
+                            <p class="card-text"><small class="text-muted">Lanzamiento: <?php echo $game['release'] ?></small></p>
                         </div>
                     </div>
-                </div>';
+                </div>
+            
+            <?php
         }
         ?>
-
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
